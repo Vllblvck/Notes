1f06757171adbb19293bc62ed1e65f7f34dd5f6d
Vllblvck <m.komar12121998@gmail.com>
2019-11-02 13:43:04 +0100

    Finished core features

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.notes;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,10 +15,7 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -24,11 +23,15 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.LinkedList;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NoteAdapter.NoteListener {
     public static final int ADD_NOTE_REQUEST = 1;
+    public static final int EDIT_NOTE_REQUEST = 2;
+    public static final String EXTRA_REQUEST = "com.example.notes.EXTRA_REQUEST";
+    public static final String EXTRA_NOTE = "com.example.notes.NOTE";
     private String notesFile = "notes.txt";
-    private LinkedList<Note> notes = new LinkedList<>();
+    private List<Note> notes = new LinkedList<>();
     private NoteAdapter noteAdapter;
 
     @Override
@@ -43,14 +46,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
-
         setSupportActionBar(toolbar);
         setTitle(R.string.app_name);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
     }
 
     private void initRecyclerView() {
-        noteAdapter = new NoteAdapter(notes);
+        noteAdapter = new NoteAdapter(notes, this);
+        noteAdapter.setNotes(notes);
+
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
 
@@ -58,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
                 layoutManager.getOrientation());
 
         dividerItemDecoration.setDrawable(getDrawable(R.drawable.separator_shape));
-        noteAdapter.setNotes(notes);
         recyclerView.addItemDecoration(dividerItemDecoration);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
@@ -70,14 +73,78 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startAddNoteActivity();
+                Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+                intent.putExtra(EXTRA_REQUEST, ADD_NOTE_REQUEST);
+                startActivityForResult(intent, ADD_NOTE_REQUEST);
             }
         });
     }
 
-    private void startAddNoteActivity() {
+    @Override
+    public void onNoteClick(int position) {
         Intent intent = new Intent(this, AddNoteActivity.class);
-        startActivityForResult(intent, ADD_NOTE_REQUEST);
+        intent.putExtra(EXTRA_REQUEST, EDIT_NOTE_REQUEST);
+        intent.putExtra(EXTRA_NOTE, notes.get(position));
+        startActivityForResult(intent, EDIT_NOTE_REQUEST);
+    }
+
+    @Override
+    public void onNoteDelete(List<Note> toDelete) {
+        for (int i = 0; i < notes.size(); i++) {
+            for (int j = 0; j < toDelete.size(); j++) {
+                if (notes.get(i).getExactCreationDate().
+                        equals(toDelete.get(j).getExactCreationDate())) {
+                    notes.remove(i);
+                }
+            }
+        }
+        saveNotes();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (resultCode == RESULT_OK) {
+            Note newNote = (Note) data.getSerializableExtra(EXTRA_NOTE);
+
+            if (requestCode == ADD_NOTE_REQUEST) {
+                notes.add(newNote);
+                Toast.makeText(getApplicationContext(), "Note added", Toast.LENGTH_SHORT).show();
+            } else {
+
+                for (int i = 0; i < notes.size(); i++) {
+                    if (notes.get(i).getExactCreationDate().equals(newNote.getExactCreationDate())) {
+                        notes.set(i, newNote);
+                        Toast.makeText(getApplicationContext(), "Note edited", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            saveNotes();
+        }
+    }
+
+    private void saveNotes() {
+        try (FileOutputStream fileOutput = this.openFileOutput(notesFile, Context.MODE_PRIVATE);
+             ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput)) {
+
+            noteAdapter.setNotes(notes);
+            objectOutput.writeObject(notes);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readNotes() {
+        try (FileInputStream fileInput = this.openFileInput(notesFile);
+             ObjectInputStream inputStream = new ObjectInputStream(fileInput)) {
+
+            notes = (LinkedList<Note>) inputStream.readObject();
+
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -95,44 +162,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-
-        if(requestCode == ADD_NOTE_REQUEST) {
-            if(resultCode == RESULT_OK) {
-                Note note = (Note) data.getSerializableExtra(AddNoteActivity.NOTE_EXTRA);
-                notes.add(note);
-                saveNotes();
-            }
-        }
-    }
-
-    private void saveNotes() {
-        try (FileOutputStream fileOutput = this.openFileOutput(notesFile, Context.MODE_PRIVATE);
-             ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput)) {
-
-            noteAdapter.setNotes(notes);
-            objectOutput.writeObject(notes);
-
-            Toast.makeText(getApplicationContext(), "Note saved", Toast.LENGTH_SHORT).show();
-
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(), "File not found", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void readNotes() {
-        try (FileInputStream fileInput = this.openFileInput(notesFile);
-             ObjectInputStream inputStream = new ObjectInputStream(fileInput)) {
-
-            notes = (LinkedList<Note>) inputStream.readObject();
-
-        } catch (IOException e) {
-           Toast.makeText(getApplicationContext(), "File not found", Toast.LENGTH_LONG).show();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 }
--- app/src/main/java/com/example/notes/NoteAdapter.java
+++ app/src/main/java/com/example/notes/NoteAdapter.java
@@ -1,20 +1,86 @@
 package com.example.notes;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.view.ActionMode;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
     private List<Note> notes;
+    private List<Note> selectedNotes = new LinkedList<>();
+    private List<CheckBox> checkBoxes = new LinkedList<>();
+    private NoteListener clickListener;
+    private boolean multiSelect = false;
+    private ActionMode actionMode;
+    private ActionMode.Callback actionModeCallback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            mode.getMenuInflater().inflate(R.menu.menu_action_mode, menu);
+            showCheckBoxes();
+            return true;
+        }
 
-    public NoteAdapter(List<Note> notes) {
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+
+            if (item.getItemId() == R.id.action_delete) {
+                clickListener.onNoteDelete(selectedNotes);
+                mode.finish();
+                return true;
+            }
+
+            if (item.getItemId() == R.id.action_select_all) {
+
+                if (selectedNotes.size() == notes.size()) {
+                    selectedNotes.clear();
+
+                    for (CheckBox checkBox : checkBoxes) {
+                        checkBox.setChecked(false);
+                    }
+
+                    return true;
+                }
+
+                selectedNotes.clear();
+                selectedNotes.addAll(notes);
+
+                for (CheckBox checkBox : checkBoxes) {
+                    checkBox.setChecked(true);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            actionMode = null;
+            multiSelect = false;
+            selectedNotes.clear();
+            hideCheckBoxes();
+        }
+    };
+
+    public NoteAdapter(List<Note> notes, NoteListener clickListener) {
         this.notes = notes;
+        this.clickListener = clickListener;
     }
 
     @NonNull
@@ -23,19 +89,7 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.note_view_holder, parent, false);
 
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        return new NoteViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
-        holder.content.setText(notes.get(position).getContent());
+        return new NoteViewHolder(view, clickListener);
     }
 
     @Override
@@ -43,17 +97,92 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
         return notes.size();
     }
 
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, final int position) {
+        holder.content.setText(notes.get(position).getContent());
+        holder.creationDate.setText(notes.get(position).getCreationDate());
+        checkBoxes.add(holder.checkBox);
+    }
+
+    private void startActionMode(View view) {
+        if (actionMode != null) {
+            return;
+        }
+
+        MainActivity activityContext = (MainActivity) view.getContext();
+        actionMode = activityContext.startSupportActionMode(actionModeCallback);
+        multiSelect = true;
+    }
+
+    private void selectItem(int position, CheckBox checkBox) {
+        Note item = notes.get(position);
+
+        if (!selectedNotes.contains(item)) {
+            selectedNotes.add(item);
+            checkBox.setChecked(true);
+        } else {
+            selectedNotes.remove(item);
+            checkBox.setChecked(false);
+        }
+    }
+
+    private void showCheckBoxes() {
+        for (CheckBox checkBox : checkBoxes) {
+            checkBox.setChecked(false);
+            checkBox.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void hideCheckBoxes() {
+        for (CheckBox checkBox : checkBoxes) {
+            checkBox.setVisibility(View.INVISIBLE);
+        }
+    }
+
     public void setNotes(List<Note> notes) {
         this.notes = notes;
         notifyDataSetChanged();
     }
 
-    public static class NoteViewHolder extends RecyclerView.ViewHolder {
-        public TextView content;
+    public interface NoteListener {
+        void onNoteClick(int position);
 
-        public NoteViewHolder(View view) {
+        void onNoteDelete(List<Note> toDelete);
+    }
+
+    public class NoteViewHolder extends RecyclerView.ViewHolder {
+        public TextView content;
+        public TextView creationDate;
+        public CheckBox checkBox;
+
+        public NoteViewHolder(final View view, final NoteListener clickListener) {
             super(view);
             content = view.findViewById(R.id.text_view_note);
+            creationDate = view.findViewById(R.id.text_view_date);
+            checkBox = view.findViewById(R.id.checkbox);
+            checkBox.setVisibility(View.INVISIBLE);
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (!multiSelect) {
+                        clickListener.onNoteClick(getAdapterPosition());
+                    } else {
+                        selectItem(getAdapterPosition(), checkBox);
+                    }
+                }
+            });
+
+            view.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    startActionMode(view);
+                    selectItem(getAdapterPosition(), checkBox);
+                    return true;
+                }
+            });
         }
     }
+
 }
+
